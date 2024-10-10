ffaac381e78afa081f9b87c13f339aaf29068123
venkatesh310599 <lakkavenkatesh1999@gmail.com>
2024-10-10 22:45:46 +0530

    Add files via upload

---
+++ 10.10.13.py
@@ -0,0 +1,10 @@
+"Given a list of numbers, write a Python program to create a new list where each element is doubled (i.e., each element is multiplied by 2)"
+l1 = [1,2,3]
+l2 = [4,5,6]
+l = (l1 + l2)
+print(l1 + l2)
+rep_list = l1 * 3
+print(rep_list)
+l = [1,2,3,4,5]
+l2 = [num * 2 for num in l]
+print(l,l2)
