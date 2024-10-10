9176bc37e5fa5253d117d8a20f5bcac1857d5f7a
venkatesh310599 <lakkavenkatesh1999@gmail.com>
2024-10-10 22:41:52 +0530

    Add files via upload

---
+++ 10.10.12.py
@@ -0,0 +1,8 @@
+".Create two lists of numbers, and use the + operator to concatenate them. Then, use the * operator to repeat the elements of one list 3 times"
+
+l1 = [1,2,3]
+l2 = [4,5,6]
+l = (l1 + l2)
+print(l1 + l2)
+rep_list = l1 * 3
+print(rep_list)
